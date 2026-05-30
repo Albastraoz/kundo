0cdaf00e5f36eba6f9755b7b677d0506425f12b6
Robin Kaal <rkaal7@gmail.com>
2026-05-30 11:54:47 +0200

    added details page

--- public/src/App.tsx
+++ public/src/App.tsx
@@ -29,9 +29,105 @@ export default function App() {
         <h1 className="text-3xl font-semibold tracking-tight mb-4">Kundo Movies</h1>
       
         {details && <div>
-          <h2 className="text-2xl font-semibold tracking-tight">This is the details page</h2>
-
           <button className="bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer" onClick={() => setDetails(null)}>Back to search</button>
+
+          <div className="flex gap-4 mt-4">
+            <div className="w-1/3 object-cover">
+              <img src={details.Poster} alt={details.Title} />
+            </div>
+            
+            <div className="flex flex-col gap-2 w-2/3">
+              <div className="flex items-center gap-2">
+                <h2 className="text-slate-400">Title: </h2>
+                <h2 className="text-2xl font-semibold tracking-tight">{details.Title}</h2>
+              </div>
+
+              <div className="flex items-center gap-2">
+                <p className="text-slate-400">Year: </p>
+                <p className="font-semibold tracking-tight">{details.Year}</p>
+              </div>
+
+              <div className="flex items-center gap-2">
+                <p className="text-slate-400">Plot: </p>
+                <p className="font-semibold tracking-tight">{details.Plot}</p>
+              </div>
+
+              <div className="flex items-center gap-2">
+                <p className="text-slate-400">Director: </p>
+                <p className="font-semibold tracking-tight">{details.Director}</p>
+              </div>
+
+              <div className="flex items-center gap-2">
+                <p className="text-slate-400">Actors: </p>
+                <p className="font-semibold tracking-tight">{details.Actors}</p>
+              </div>
+
+              <div className="flex items-center gap-2">
+                <p className="text-slate-400">Genre: </p>
+                <p className="font-semibold tracking-tight">{details.Genre}</p>
+              </div>
+
+              <div className="flex items-center gap-2">
+                <p className="text-slate-400">Language: </p>
+                <p className="font-semibold tracking-tight">{details.Language}</p>
+              </div>
+
+              <div className="flex items-center gap-2">
+                <p className="text-slate-400">Country: </p>
+                <p className="font-semibold tracking-tight">{details.Country}</p>
+              </div>
+
+              <div className="flex items-center gap-2">
+                <p className="text-slate-400">Awards: </p>
+                <p className="font-semibold tracking-tight">{details.Awards}</p>
+              </div>
+
+              <div className="flex items-center gap-2">
+                <p className="text-slate-400">Ratings: </p>
+                <p className="font-semibold tracking-tight">{details.Ratings.map((rating) => rating.Source).join(", ")}</p>
+              </div>
+
+              <div className="flex items-center gap-2">
+                <p className="text-slate-400">Metascore: </p>
+                <p className="font-semibold tracking-tight">{details.Metascore}</p>
+              </div>
+
+              <div className="flex items-center gap-2">
+                <p className="text-slate-400">imdbRating: </p>
+                <p className="font-semibold tracking-tight">{details.imdbRating}</p>
+              </div>
+
+              <div className="flex items-center gap-2">
+                <p className="text-slate-400">imdbVotes: </p>
+                <p className="font-semibold tracking-tight">{details.imdbVotes}</p>
+              </div>
+
+              <div className="flex items-center gap-2">
+                <p className="text-slate-400">Type: </p>
+                <p className="font-semibold tracking-tight">{details.Type}</p>
+              </div>
+
+              <div className="flex items-center gap-2">
+                <p className="text-slate-400">DVD: </p>
+                <p className="font-semibold tracking-tight">{details.DVD}</p>
+              </div>
+
+              <div className="flex items-center gap-2">
+                <p className="text-slate-400">BoxOffice: </p>
+                <p className="font-semibold tracking-tight">{details.BoxOffice}</p>
+              </div>
+
+              <div className="flex items-center gap-2">
+                <p className="text-slate-400">Production: </p>
+                <p className="font-semibold tracking-tight">{details.Production}</p>
+              </div>
+
+              <div className="flex items-center gap-2">
+                <p className="text-slate-400">Website: </p>
+                <p className="font-semibold tracking-tight">{details.Website}</p>
+              </div>
+            </div>
+          </div>
         </div>}
 
         {!details && <div>
